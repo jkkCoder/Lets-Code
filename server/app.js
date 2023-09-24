183bac668bf403ef923b7e0bf1ec65bd3b2448d8
jkkCoder <jayeshk118@gmail.com>
2023-09-24 18:33:48 +0530

    mongoose connection established

--- server/app.js
+++ server/app.js
@@ -1,12 +1,37 @@
 import express from 'express'
+import 'dotenv/config'
+import mongoose from 'mongoose'
 const app = express()
 
 const PORT = 5000
 
-app.get('/',(req,res) => {
-    res.send('hello world')
+app.use(express.json())
+const dbURL = `mongodb+srv://jayeshk118:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.qgq3dkm.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.connect(dbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+  app.use((req, res, next) => {
+    res.status(500).send('Internal Server Error');
+  })
 })
 
-app.listen(PORT, () => {
-    console.log('app running at port ', PORT)
-})
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+
+    app.get('/', (req, res) => {
+        res.status(200).send('Hello, World!');
+    });
+
+        
+    app.listen(PORT, () => {
+        console.log('app running at port ', PORT)
+    })
+});
