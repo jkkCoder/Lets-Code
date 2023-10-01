43df9143e0ec4aa4548facaeba37d40a4b08f1fe
jkkCoder <jayeshk118@gmail.com>
2023-10-01 16:52:19 +0530

    cors and proxy setup b/w fe and be

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react' 
-
+import axios from 'axios'
 
 const Login = () => {
     const [isSignIn, setSignIn] = useState(false);
@@ -13,21 +13,29 @@ const Login = () => {
         setPassWord("")   
         setSignIn(prev => !prev)             
     }
+
+    const handleCta = async () => {
+        const response = await axios.post('/user/login', {
+            loginField: emailAddress,
+            password: 'test123'
+        })
+    }
     
     return (
     <div>
         <h1>LetsCode</h1>
         <h2>{isSignIn ? 'Sign In' : 'Sign Up'}</h2>
-               
-        {!isSignIn && (<>            
-            <input value={fullName} onChange={(e)=> setFullName(e.target.value)}type="text" placeholder='Full Name'/>
-            <input value={userName} onChange={(e)=> setUserName(e.target.value)} type="text" placeholder='User Name'/>            
-        </>)}
-       
-        <input value={emailAddress} onChange={(e)=>setEmailAddress(e.target.value)} type="text" placeholder={isSignIn ? 'Email address or User Name' : 'Email address'}/>
-        <input value={passWord} onChange={(e)=>setPassWord(e.target.value)} type="password" placeholder='Password'/>
-        <button>{isSignIn ? 'Sign In' : 'Sign Up'}</button>
-        <span onClick={toggleSignInForm}>{isSignIn ? "New to LetsCode ? Sign Up Now" : "Already a User ? Sign In"}</span>
+        <form onSubmit={e =>  e.preventDefault() }>
+            {!isSignIn && (<>            
+                <input value={fullName} onChange={(e)=> setFullName(e.target.value)}type="text" placeholder='Full Name'/>
+                <input value={userName} onChange={(e)=> setUserName(e.target.value)} type="text" placeholder='User Name'/>            
+            </>)}
+        
+            <input value={emailAddress} onChange={(e)=>setEmailAddress(e.target.value)} type="text" placeholder={isSignIn ? 'Email address or User Name' : 'Email address'}/>
+            <input value={passWord} onChange={(e)=>setPassWord(e.target.value)} type="password" placeholder='Password'/>
+            <button onClick={handleCta}>{isSignIn ? 'Sign In' : 'Sign Up'}</button>
+            <span onClick={toggleSignInForm}>{isSignIn ? "New to LetsCode ? Sign Up Now" : "Already a User ? Sign In"}</span>
+        </form>
     </div>
   )
 }
